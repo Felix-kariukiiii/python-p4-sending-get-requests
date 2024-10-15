209c9639ab99abeeeeab2a2bccc91c8844ae560a
Felix <felix@vasty.localdomain>
2024-10-15 15:43:19 +0300

    completed

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,14 @@
 import requests
 import json
 
+# urlText = "https://learn-co-curriculum.github.io/json-site-example/"
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
+# responseText= requests.get(urlText)
 response = requests.get(url)
 
+# print(responseText.text)
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+print(json.dumps(json_content, indent=4, sort_keys=True))
